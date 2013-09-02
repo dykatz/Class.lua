e7987add06853a94e5b31a6e02b8ede58cd24492
muddmaker <djshredd@gmail.com>
2013-09-02 11:39:59 -0700

    Update class.lua
    fixed some memory management issues, as well as improvements to multiple inheritance
    
    there is, however, a big issue with calling methods, which may not be fixed for a while

--- class.lua
+++ class.lua
@@ -1,26 +1,41 @@
-function class(...)
-	local m = {}
-	local c = setmetatable({}, m)
-	m.__index = m
-	c.bases = {...}
-	for _, base in ipairs(c.bases) do
-		if type(base) == 'table' then
-			for tag, val in pairs(base) do
-				local newtag = tag
-				while c[newtag] do
-					newtag = newtag .. '_'
-				end
-				c[newtag] = val
+local m = {}
+m.__index = m
+function m:isinstance(obj)
+	if getmetatable(obj) == self then
+		return true
+	else
+		for _, child in ipairs(self.__children) do
+			if child:isinstance(obj) then
+				return true
 			end
 		end
 	end
-	function m:isinstance(obj)
-		return getmetatable(obj) == self
-	end
-	function m:__call(...)
-		local obj = setmetatable({}, c)
-		if obj.init then obj:init(...) end
-		return obj
+	return false
+end
+function m:__call(...)
+	local obj = setmetatable({}, self)
+	if obj.init then obj:init(...) end
+	return obj
+end
+function class(...)
+	local c = setmetatable({}, m)
+	rawset(c, '__bases', {...})
+	rawset(c, '__children', {})
+	for _, base in ipairs(c.__bases) do
+		if type(base) == 'table' then
+			for tag, val in pairs(base) do
+				if tag ~= '__bases' and tag ~= '__children' then
+					local newtag = tag
+					while rawget(c, newtag) do
+						newtag = newtag .. '_'
+					end
+					rawset(c, newtag, val)
+				end
+			end
+			if type(rawget(base, '__children')) == 'table' then
+				rawset(rawget(base, '__children'), #rawget(base, '__children') + 1, self)
+			end
+		end
 	end
 	function c.__index(obj, key)
 		if type(rawget(c, key)) == 'function' then
@@ -38,7 +53,7 @@ function class(...)
 				return rawget(rawget(c, key), 'set')(obj, val)
 			end
 		end
-		return rawset(obj, key, val)
+		return rawget(rawset(obj, key, val), key)
 	end
 	return c
 end
